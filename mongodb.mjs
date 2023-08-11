b29772e1086ff0daa08432f9eb485d37c555a5a9
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-08-11 03:33:33 -0700

    errors

--- mongodb.mjs
+++ mongodb.mjs
@@ -1,24 +1,22 @@
 import { MongoClient } from 'mongodb'
 
-const uri = `mongodb+srv://ahadsts9901:ahsan123@cluster0.2ib77co.mongodb.net/?retryWrites=true&w=majority`
-
-export const client = new MongoClient(uri)
+const uri = 'mongodb+srv://ahadsts9901:ahsan123@cluster0.2ib77co.mongodb.net/?retryWrites=true&w=majority';
+export const client = new MongoClient(uri);
 
 async function run() {
     try {
-        await client.connect()
-        console.log("successfully connected")
+        await client.connect();
+        console.log("Successfully connected to Atlas");
     } catch (err) {
-        console.log(err.stack)
-        await client.close()
+        console.log(err);
+        await client.close();
         process.exit(1)
     }
 }
+run().catch(console.dir);
 
-run().catch(console.dir)
-
-process.on('SIGNINT', async function() {
-    console.log("app is terminating")
-    await client.close()
-    process.exit(0)
-})
+process.on('SIGINT', async function () {
+    console.log("app is terminating");
+    await client.close();
+    process.exit(0);
+});
--- server.mjs
+++ server.mjs
@@ -1,19 +1,50 @@
-import express from 'express'
+import express from 'express';
 import path from 'path';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config'
 const __dirname = path.resolve();
 
-import apiv1Router from './apiv1/main.mjs'
+import authRouter from './routes/auth.mjs'
+// import postRouter from './routes/post.mjs'
+import cookieParser from 'cookie-parser'
+import { decode } from 'punycode';
 
-const app = express()
-
+const app = express();
 app.use(express.json()); // body parser
+app.use(cookieParser()); // cookie parser
 
-app.use("/api/v1", apiv1Router)
+
+// /api/v1/login
+app.use("/api/v1", authRouter)
+
+app.use((req, res, next) => {
+    console.log("cookies: ", req.cookies);
+
+    const token = req.cookies.token;
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET);
+        console.log("decoded: ", decoded);
+
+        req.body.decoded = {
+            firstName: decoded.firstName,
+            lastName: decoded.lastName,
+            email: decoded.email,
+            isAdmin: decoded.isAdmin,
+        };
+
+        next();
+
+    } catch (err) {
+        res.status(401).send({ message: "invalid token" })
+    }
+
+})
+
+// app.use("/api/v1", postRouter)
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+    console.log(`Example server listening on port ${PORT}`)
 })
